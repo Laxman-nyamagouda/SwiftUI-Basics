0d63e581b4b272b39bc0486426e731a5f79738f8
Laxman Nyamagouda <laxman.nyamagouda@gmail.com>
2022-11-23 00:49:04 -0500

    Initial Commit

---
+++ SwiftUI-Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI-Basics
+//
+//  Created by Laxman Nyamagouda on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
